8e6b33ae1dadd12cd80eb49453ee6dd7a80706a5
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2023-10-26 12:34:43 -0300

    tentativa de conexão com o Postman

--- WebAPI/Models/Filme.cs
+++ WebAPI/Models/Filme.cs
@@ -1,9 +1,21 @@
 namespace WebAPI.Models;
 
+using System.ComponentModel.DataAnnotations; 
+
+/* DATA ANNOTATIONS
+require-> campo obrigatorio
+range-> intervalo(min, max)
+*/
+
 public class Filme 
 {
+  [Required]
   public string Titulo { get; set; }
+  [Required]
+  [MaxLength(50)]
   public string Genero { get; set; }
+  [Required]
+  [Range(70, 600)]
   public int Duracao { get; set; }
   
 }
