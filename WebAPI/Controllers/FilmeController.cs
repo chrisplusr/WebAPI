38c6829b68edfb2781a5d917a7c9678e774dfdb8
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2023-10-25 21:32:30 -0300

    avançando no curso

--- WebAPI/Controllers/FilmeController.cs
+++ WebAPI/Controllers/FilmeController.cs
@@ -5,14 +5,25 @@ using WebAPI.Models;
 
 namespace WebAPI.Controllers;
 
-//[Route("[controller]")]  -> Token que será substituído pelo nome do controlador ao qual o atributo está associado. Isso permite que o roteador gere automaticamente URLs com base no nome do controlador.
+//[Route("[controller]")]  -> Token que será substituído pelo nome do controlador ao qual o atributo está associado. 
+//Isso permite que o roteador gere automaticamente URLs com base no nome do controlador.
 [ApiController]
 [Route("[controller]")] 
 public class FilmeController : ControllerBase 
 {
   private static List<Filme> listaDeFilmes = new List<Filme>();
-  public void AdicionaFilme(Filme filme) 
+
+  /*
+    Para inserir uma nova informação no banco de dados usamos o post(HttpPost).
+    O método GET é usado para recuperar informações do servidor,
+    enquanto o método POST é usado para enviar dados ao servidor,
+  */
+  [HttpPost]
+  public void AdicionaFilme([FromBody] Filme filme) 
   {
-    filme.Add(filme);
+    //o parâmetro filme vem através do corpo da requisição
+    listaDeFilmes.Add(filme);
+    Console.WriteLine(filme.Titulo);
+    Console.WriteLine(filme.Duracao);
   }
 }
